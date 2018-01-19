5622a61e3431a336d286d05a0a594622578ecd22
Kat Balayan <mkncbalayan@gmail.com>
2018-01-19 18:19:14 +0800

    2

--- index.php
+++ index.php
@@ -1,30 +1,27 @@
 <?php
 
-require_once 'assets/lib/12days.php';
+require_once 'assets/lib/twelve_days.php';
 
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
 	<title><?php echo getTitle(); ?> Lyrics</title>
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="">
+
 </head>
 <body>
-
-	<div class="wrapper">
 
 	<?php
 
 	echo getLyrics();
 
 	?>
-
-	</div>
 
 	<script type="text/javascript">
 		// get all day containers
@@ -35,10 +32,10 @@ require_once 'assets/lib/12days.php';
 			if (index > 0)
 				document.getElementById('day' + index).style.display = 'none';
 		});
-		
 	</script>
 
-	<script type="text/javascript" src="assets/lib/jquery-3.2.1x.slim.min.js"></script>
+	<script type="text/javascript" src="assets/lib/jquery-3.2.1.min.js"></script>
+
 	<script type="text/javascript" src="assets/js/animate_days.js"></script>
 
 </body>
